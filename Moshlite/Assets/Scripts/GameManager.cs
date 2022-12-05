7227d965bc17cf7fab6b425a8b64247a1ab78772
Scott King <jsking@andrew.cmu.edu>
2022-12-04 19:14:58 -0500

    Improve ui

--- Moshlite/Assets/Scripts/GameManager.cs
+++ Moshlite/Assets/Scripts/GameManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [Header("Audio")]
     [SerializeField] private List<Song> songs = new List<Song>();
     [SerializeField] private AudioSource audioSource;
+    private Song curSong = null;
 
     [Header("UI")]
     [SerializeField] private GameObject titlePanel;
     [SerializeField] private GameObject songSelectionPanel;
+    [SerializeField] private Image fadeToBlackImage;
+    
+    [SerializeField] private GameObject songTitlePanel;
+    [SerializeField] private TextMeshProUGUI songTitleText;
+    [SerializeField] private TextMeshProUGUI songArtistText;
+    [SerializeField] private TextMeshProUGUI timeText;
 
     // Game state
-    private enum GameState { title, songSelection, playingSong, playingFreeplay };
+    private enum GameState { title, songSelection, playingSong, playingFreeplay, fadingToReset };
     private GameState curState = GameState.title;
 
     private bool receivedInputThisFrame = false; 
@@ -29,9 +38,16 @@ public class GameManager : MonoBehaviour
 
         songSelectionPanel.SetActive(false);
         titlePanel.SetActive(true);
+        songTitlePanel.SetActive(false);
+        songTitleText.text = "";
+        songArtistText.text = "";
+        timeText.text = "";
         curState = GameState.title;
         idleTimer = 0.0f;
         receivedInputThisFrame = false;
+
+        fadeToBlackImage.enabled = true;
+        fadeToBlackImage.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
 
         audioSource.Stop();
         audioSource.loop = false;
@@ -62,9 +78,10 @@ public class GameManager : MonoBehaviour
         {
             idleTimer += Time.deltaTime;
 
-            if (idleTimer > timeToIdle)
+            if (idleTimer > timeToIdle && curState != GameState.fadingToReset)
             {
-                ResetScene(); // TODO: should probably reset in a less jarring way in this case
+                curState = GameState.fadingToReset;
+                StartCoroutine(FadeOnIdleReset());
             }
         }
         
@@ -88,7 +105,8 @@ public class GameManager : MonoBehaviour
             // Reset when song is over
             if (!audioSource.isPlaying)
             {
-                ResetScene();
+                curState = GameState.fadingToReset;
+                StartCoroutine(FadeToCredits(curSong));
             }
         }
         else if (curState == GameState.playingFreeplay)
@@ -107,10 +125,13 @@ public class GameManager : MonoBehaviour
         
         if (songListIndex < songs.Count)
         {
-            audioSource.clip = songs[songListIndex].clip;
+            songSelectionPanel.SetActive(false);
+
+            curSong = songs[songListIndex];
+            audioSource.clip = curSong.clip;
             audioSource.Play();
 
-            songSelectionPanel.SetActive(false);
+            StartCoroutine(FadeToPlayMode(curSong));
         }
     }
 
@@ -118,6 +139,91 @@ public class GameManager : MonoBehaviour
     {
         curState = GameState.playingFreeplay;
         songSelectionPanel.SetActive(false);
+        curSong = null;
+
+        StartCoroutine(FadeToPlayMode());
+    }
+
+    public IEnumerator FadeToPlayMode(Song song = null)
+    {
+        float cur_alpha = 0.0f;
+
+        // Fade to black
+        while (cur_alpha < 0.99f)
+        {
+            cur_alpha = Mathf.Lerp(cur_alpha, 1.0f, Time.deltaTime * 2.0f);
+            fadeToBlackImage.color = new Color(fadeToBlackImage.color.r, fadeToBlackImage.color.g, fadeToBlackImage.color.b, cur_alpha);
+            yield return null;
+        }
+        fadeToBlackImage.color = new Color(0, 0, 0, 1);
+        yield return new WaitForSeconds(0.5f);
+
+        // Title card
+        if (song == null)
+        {
+            songTitleText.text = "";
+            songArtistText.text = "";
+        }
+        else
+        {
+            songTitleText.text = '"' + song.title.ToUpper() + '"';
+            songArtistText.text = "   " + song.artist;
+        }
+        timeText.text = System.DateTime.Now.ToString() + ", FINAL GAMES";
+        songTitlePanel.SetActive(true);
+        yield return new WaitForSeconds(4.0f);
+
+        fadeToBlackImage.color = new Color(0, 0, 0, 0);
+        songTitlePanel.SetActive(false);
+    }
+
+    public IEnumerator FadeToCredits(Song song = null)
+    {
+        float cur_alpha = 0.0f;
+
+        // Fade to black
+        while (cur_alpha < 0.99f)
+        {
+            cur_alpha = Mathf.Lerp(cur_alpha, 1.0f, Time.deltaTime * 2.0f);
+            fadeToBlackImage.color = new Color(fadeToBlackImage.color.r, fadeToBlackImage.color.g, fadeToBlackImage.color.b, cur_alpha);
+            yield return null;
+        }
+        fadeToBlackImage.color = new Color(0, 0, 0, 1);
+        yield return new WaitForSeconds(0.5f);
+
+        // Credits
+        if (song == null)
+        {
+            songTitleText.text = "";
+            songArtistText.text = "";
+        }
+        else
+        {
+            songTitleText.text = '"' + song.title.ToUpper() + '"';
+            songArtistText.text = "   " + song.artist;
+        }
+        timeText.text = System.DateTime.Now.ToString() + ", FINAL GAMES";
+        songTitlePanel.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+
+        ResetScene();
+    }
+
+    public IEnumerator FadeOnIdleReset()
+    {
+        float cur_alpha = 0.0f;
+
+        // Fade to black
+        while (cur_alpha < 0.99f)
+        {
+            cur_alpha = Mathf.Lerp(cur_alpha, 1.0f, Time.deltaTime * 2.0f);
+            fadeToBlackImage.color = new Color(fadeToBlackImage.color.r, fadeToBlackImage.color.g, fadeToBlackImage.color.b, cur_alpha);
+            yield return null;
+        }
+        fadeToBlackImage.color = new Color(0, 0, 0, 1);
+        yield return new WaitForSeconds(0.5f);
+
+        ResetScene();
     }
 
     private void ResetScene()
@@ -127,7 +233,7 @@ public class GameManager : MonoBehaviour
 }
 
 [System.Serializable]
-public struct Song
+public class Song
 {
     public string title;
     public string artist;
