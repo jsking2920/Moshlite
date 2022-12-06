d0c32c5a8909297db630b7d9cb46a10825fb5f9d
Scott King <jsking@andrew.cmu.edu>
2022-12-06 17:56:21 -0500

    Work on character controller

--- Moshlite/Assets/Scripts/CharacterController.cs
+++ Moshlite/Assets/Scripts/CharacterController.cs
@@ -4,19 +4,81 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private float drag = 0.1f;
-    // Start is called before the first frame update
+    [Header("Body Part Rigidbodies")]
+    [SerializeField] private Rigidbody hips;
+    [SerializeField] private Rigidbody chest;
+    [SerializeField] private Rigidbody head;
+
+    [SerializeField] private Rigidbody rightUpperArm;
+    [SerializeField] private Rigidbody rightForeArm;
+    [SerializeField] private Rigidbody rightHand;
+    [SerializeField] private Rigidbody rightIndex;
+    [SerializeField] private Rigidbody rightFingers;
+    [SerializeField] private Rigidbody leftUpperArm;
+    [SerializeField] private Rigidbody leftForeArm;
+    [SerializeField] private Rigidbody leftHand;
+    [SerializeField] private Rigidbody leftIndex;
+    [SerializeField] private Rigidbody leftFingers;
+
+    [SerializeField] private Rigidbody rightUpperLeg;
+    [SerializeField] private Rigidbody rightLowerLeg;
+    [SerializeField] private Rigidbody rightAnkle;
+    [SerializeField] private Rigidbody rightFoot;
+    [SerializeField] private Rigidbody leftUpperLeg;
+    [SerializeField] private Rigidbody leftLowerLeg;
+    [SerializeField] private Rigidbody leftAnkle;
+    [SerializeField] private Rigidbody leftFoot;
+
+    private List<Rigidbody> rbList;
+
+    [Header("Physics Scalars")]
+    // Scalars applied to all body parts at start
+    [SerializeField] private float dragScalar = 1.0f; 
+    [SerializeField] private float massScalar = 1.0f;
+
+    [Header("Persistent Forces")]
+    [SerializeField] private Vector3 chestForce = new Vector3(0.0f, 800.0f, 0.0f);
+    [SerializeField] private Vector3 headForce = new Vector3(0.0f, 100.0f, 0.0f);
+    [SerializeField] private Vector3 feetForce = new Vector3(0.0f, -200.0f, 0.0f);
+
     void Start()
     {
-        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        rbList = new List<Rigidbody>{ hips, chest, head, rightUpperArm, rightForeArm, rightHand, rightIndex, rightFingers,
+                                      leftUpperArm, leftForeArm, leftHand, leftIndex, leftFingers, rightUpperLeg, rightLowerLeg, 
+                                      rightAnkle, rightFoot, leftUpperLeg, leftLowerLeg, leftAnkle, leftFoot };
+        foreach (Rigidbody rb in rbList)
         {
-            rb.drag = drag;
+            rb.drag *= dragScalar;
+            rb.mass *= massScalar;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Apply persistent forces
+        chest.AddForce(chestForce);
+        head.AddForce(headForce);
+
+        leftAnkle.AddForce(feetForce);
+        rightAnkle.AddForce(feetForce);
+    }
+
+    public void Jump()
+    {
+        hips.AddForce(new Vector3(0.0f, 400.0f, 0.0f), ForceMode.Impulse);
+    }
+
+    public void BangHead()
+    {
+        head.AddForce(new Vector3(0.0f, -50.0f, 50.0f), ForceMode.Impulse);
+        chest.AddForce(new Vector3(0.0f, 0.0f, -25.0f));
+    }
+
+    public void ScaleMass(float scalar)
+    {
+        foreach (Rigidbody rb in rbList)
+        {
+            rb.mass *= massScalar;
+        }
     }
 }
--- Moshlite/Assets/Scripts/CharacterManager.cs
+++ Moshlite/Assets/Scripts/CharacterManager.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    [SerializeField] private List<CharacterController> chars;
 
+    [SerializeField] private Vector3 gravity = new Vector3(0.0f, -9.81f, 0.0f);
+
+    private void Start()
+    {
+        Physics.gravity = gravity;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (CharacterController c in chars)
+            {
+                c.BangHead();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            foreach (CharacterController c in chars)
+            {
+                c.Jump();
+            }
+        }
+    }
 }
